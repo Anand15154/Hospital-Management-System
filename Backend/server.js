4ada9e27b1736c6d9de3c975720c6f35a20d5370
Anand Tiwari <106400109+Anand15154@users.noreply.github.com>
2024-04-20 12:54:53 +0530

    Model updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,14 @@
 import app from "./app.js";
+import cloudinary from "cloudinary";
+
+cloudinary.v2.config({
+    clod_name: process.env.COLUDINARY_CLOUD_NAME,
+    api_key: process.env.COLUDINARY_API_KEY,
+    api_secret: process.env.COLUDINARY_API_SECRET,
+
+})
+
+
 
 app.listen(process.env.PORT, () => {
     console.log(`server started at port:4000`);
